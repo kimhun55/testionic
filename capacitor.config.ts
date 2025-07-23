671d06e49b125c474c78052838e05bd57a0d9c0d
chaiyaporn wama <chaiyaporn.w@dek-d.com>
2025-07-23 14:08:28 +0700

    update android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.myapp',
-  appName: 'myapp',
+  appId: 'com.kimhun55.ionic',
+  appName: 'TestIonic',
   webDir: 'dist'
 };
 
